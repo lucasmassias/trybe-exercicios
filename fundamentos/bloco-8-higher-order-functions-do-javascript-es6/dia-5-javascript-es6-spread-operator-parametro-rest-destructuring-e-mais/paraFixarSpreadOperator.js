81f539938dfc1031defeb315bfac0845b6a70517
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-09-20 21:58:56 -0300

    modified

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/paraFixarSpreadOperator.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/paraFixarSpreadOperator.js
@@ -8,10 +8,9 @@ const specialFruit = ['morango', 'manga', 'laranja'];
 // Faça uma lista de complementos que você gostaria de adicionar
 const additionalItens = ['leite condensado', 'leite em pó', 'sucrilhos'];
 
-const fruitSalad = (...fruit, ...additional) => {
+const fruitSalad = (fruit, additional) => {
   // Esreva sua função aqui
-  const getSalad =  ({ ...fruit ...additional });
-  return getSalad;
-};
+ 
+}
 
 console.log(fruitSalad(specialFruit, additionalItens));
