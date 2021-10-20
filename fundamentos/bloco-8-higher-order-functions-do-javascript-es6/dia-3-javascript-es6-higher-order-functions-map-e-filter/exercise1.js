910c4dfd74ff89ba498ac997e87e4bb4540b9e99
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-10-20 18:19:02 -0300

    implmt: ex1 using map

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -77,6 +77,7 @@ const expectedResult = [
   
   function formatedBookNames() {
     // escreva seu código aqui
+    return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
   }
   
   assert.deepStrictEqual(formatedBookNames(), expectedResult);
