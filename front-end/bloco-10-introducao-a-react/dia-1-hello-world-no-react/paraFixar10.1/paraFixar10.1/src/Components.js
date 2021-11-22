bbd2d5ca934ef01d34e6b54739ee7055079a27b8
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-11-22 18:27:51 -0300

    para fixar correto do dia 10.1

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/paraFixar10.1/paraFixar10.1/src/Components.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Components extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Lucas Massias Freitas</h1>
+            <p>Aluno de desenvolvimento Full-stack na Trybe</p>
+        </div>
+    )
+  }
+}
+
+export default Components;
