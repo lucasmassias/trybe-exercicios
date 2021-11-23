225d1d01d5c4f67e9ab4d25e079f555f02500f16
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-11-23 15:33:24 -0300

    add altenativeText no app fixation-exercises-10-2

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -5,7 +5,7 @@ import Image from './Image';
 class App extends React.Component {
   render() {
     return (
-      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'/>
+      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
     );
   } 
    
