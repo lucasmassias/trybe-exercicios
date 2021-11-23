799156ca770d795581f15929f739b0b39d3a1e36
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-11-23 02:29:52 -0300

    implmt componetes Content.jsx e Footer.jsx, finaliza 10.1 :rocket:

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/agora-pratica-ex3/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/agora-pratica-ex3/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Content from './Content';
+import Footer from './Footer';
 import Header from './Header';
 
 class App extends React.Component {
   render() {
     return (
-      <Header/>
+      <div>
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
     );
   }
 }
