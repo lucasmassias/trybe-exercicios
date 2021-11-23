c11904ac4388df31ea9379ccd5186451b27abaa7
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-11-23 17:53:24 -0300

    implmtado no mesmo app os parafixar dos conteudos props, composiçao de componentes e lista de componentes e chaves

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
-import Image from './Image';
+import ImageCat from './ImageCat';
 import Order from './Order';
+import UserProfile from './UserProfile';
 
 class App extends React.Component {
   render() {
+
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      }
+    ];
+
     const headphone = {
       id: 102,
       user: "cena@gmail.com",
@@ -26,11 +43,13 @@ class App extends React.Component {
     };
 
     return (
+      
       <div className="App">
         <h1> Orders recently created </h1>
+         <ImageCat source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
          <Order order = {headphone}/>
          <Order order = {energyDrink}/>
-         <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
+         { users.map(user => <UserProfile key={user.id} user={user} /> )}
       </div>
     );
   } 
--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/ImageCat.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/ImageCat.js
@@ -1,10 +1,9 @@
-// arquivo Image.js
 import React from 'react';
 
-class Image extends React.Component {
+class ImageCat extends React.Component {
   render() {
     return <img src={this.props.source} alt={this.props.alternativeText} />;
   }
 }
 
-export default Image;
+export default ImageCat;
