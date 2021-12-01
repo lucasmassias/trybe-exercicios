448320c3930ed401bcb7ba2b1029569a2643dd94
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-12-01 18:26:28 -0300

    recomeço do para-fixar 11-2 apos app anteriormente instalado ter dado problema ao executar npm start

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/para-fixar-11-2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1> Para Fixar 11.2</h1>
+      </>
+    );
+  }
+}
+
+export default App;
