f7d4342fb3a9fe54a16750ec9f4f30ec31d64b56
lucasmassias <lucasmassiasfreitas@gmail.com>
2021-12-02 16:44:20 -0300

    impmnt 4, 5 e 6 do para fixar 11.2 event handles genericos

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/para-fixar-11-2/src/Form.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/para-fixar-11-2/src/Form.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-
 class Form extends Component {
   constructor() {
     super();
 
-    
+
     this.state = {
-      estadoFavorito: '',
+      estado: '',
       email: '',
       write: '',
       age: '',
@@ -16,7 +15,7 @@ class Form extends Component {
 
     this.handleChange = this.handleChange.bind(this);
   }
-  
+
 
   handleChange({ target }) {
     const { name } = target;
@@ -28,81 +27,94 @@ class Form extends Component {
   }
 
   render() {
-    const { estadoFavorito, email, write, age, name, vaiComparecer } = this.state;
+    const { estado, email, write, age, name, vaiComparecer } = this.state;
 
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
         <form className="form">
 
-          <label htmlFor='favState'>
-            Estado favorito:
-            <textarea
-              id='estadoFavorito'
-              name="estadoFavorito"
-              type='text'
-              value={ estadoFavorito }
-              onChange={ this.handleChange }
+          <fieldset>
+            <legend> Informações pessois</legend>
+
+            <label htmlFor="name">
+              Nome:
+              <input
+                id="name"
+                name="name"
+                type="text"
+                onChange={this.handleChange}
+                value={name}
               />
-          </label>
-
-          <label htmlFor="name">
-            Nome:
-            <input
-              id="name"
-              name="name"
-              type="text"
-              onChange={ this.handleChange }
-              value={ name }
-            />
-          </label>
-
-          <label htmlFor='email'>
-            Email:
-            <input
-              id="email"
-              name="email"
-              tyoe="email"
-              onChange={ this.handleChange }
-              value={ email }
-            />
-          </label>
-
-          <label htmlFor='age'>
-            Idade:
-            <select
-              id="age"
-              name="age"
-              onChange={ this.handleChange }
-              value={ age }
-            >
-              <option value="adult">Maior que 18</option>
-              <option value="underage">Menor que 18</option>
-            </select>
-          </label>
-
-          <label htmlFor='write'>
-            Escreva o que quiser:
-            <textarea
-              id='write'
-              name='write'
-              onChange={ this.handleChange }
-              value={ write }
-            />
-          </label>
-
-          <label>
-           
-            <input 
-              id='vaiComparecer'
-              name="vaiComparecer"
-              type="checkbox"
-              onChange={ this.handleChange }
-              value={ vaiComparecer }
-            />
-            Vai comparecer?
             </label>
 
+            <label htmlFor='fState'>
+              Estado:
+              <input
+                id='estado'
+                name="estado"
+                type='text'
+                value={estado}
+                onChange={this.handleChange}
+              />
+            </label>
+
+            <label htmlFor='email'>
+              Email:
+              <input
+                id="email"
+                name="email"
+                tyoe="email"
+                onChange={this.handleChange}
+                value={email}
+              />
+            </label>
+
+              <label htmlFor='age'>
+              Idade:
+              <select
+                id="age"
+                name="age"
+                onChange={this.handleChange}
+                value={age}
+                >
+                <option value="adult">Maior que 18</option>
+                <option value="underage">Menor que 18</option>
+              </select>
+            </label>
+
+          </fieldset>
+
+          <fieldset>
+            <legend> Diga se irá comparecer e compartilhe o que achar necessário </legend>
+
+            <label htmlFor='write'>
+              Escreva o que quiser:
+              <textarea
+                id='write'
+                name='write'
+                onChange={this.handleChange}
+                value={write}
+              />
+            </label>
+
+            <label>
+
+              <input
+                id='vaiComparecer'
+                name="vaiComparecer"
+                type="checkbox"
+                onChange={this.handleChange}
+                value={vaiComparecer}
+              />
+              Vai comparecer?
+            </label>
+
+            <label>
+              Envie o que quiser:
+              <input type="file" />
+            </label>
+          </fieldset>
         </form>
       </div>
     );
